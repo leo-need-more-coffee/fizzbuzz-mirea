60154b9bb9e2521dc50fc986c8e9ef5fb9368441
pzrnqt1vrss <pzrqt1vrss>
2023-09-20 23:49:56 +0300

    fix

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
-#include <stdint.h>
-#include <stddef.h>
 #include <pthread.h>
-#include <math.h>
 
 #define NUM_THREADS 32
 
